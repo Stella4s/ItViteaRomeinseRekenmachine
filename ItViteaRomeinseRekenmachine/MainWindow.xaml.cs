6db710db58c9d764bf8f691e44887aec7ee32a13
Stella van der burg <estellavanderburg@gmail.com>
2020-04-08 13:51:55 +0200

    Added Variables for roman numbers and arabic counterparts. Added Enums & Corresponding switch to keep track of which operators were selected. Made number buttons, operator buttons and = button function.

--- ItViteaRomeinseRekenmachine/MainWindow.xaml.cs
+++ ItViteaRomeinseRekenmachine/MainWindow.xaml.cs
@@ -20,151 +20,75 @@ namespace ItViteaRomeinseRekenmachine
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region private Variables
+        private string RomanNum1 = null, RomanNum2, strInputDisplay, RomanResult;
+        private int ArabicNum1 = 0, ArabicNum2, ArabicResult;
+        private bool IsSecondNumber = false;
+        private NumeralConversion Conversion;
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
+            Conversion = new NumeralConversion();
         }
 
-        #region Conversion methods
-        public static Dictionary<char, int> RomanArabicDict =
-        new Dictionary<char, int>
+        /// <summary>
+        /// Methods that aren't directly tied to any object handler, but instead serve to support those methods.
+        /// </summary>
+        #region Support Methods
+        private void UpdateNumbers()
         {
-                {'I',   1 },
-                {'V',   5 },
-                {'X',   10 },
-                {'L',   50 },
-                {'C',   100 },
-                {'D',   500 },
-                {'M',   1000 },
-        };
-        public static Dictionary<int, string> ArabicRomanDict =
-            new Dictionary<int, string>
-            {
-                { 1000,  "M" },
-                { 900,   "CM" },
-                { 500,   "D" },
-                { 400,   "CD" },
-                { 100,   "C" },
-                { 90,    "XC" },
-                { 50,    "L" },
-                { 40,    "XL" },
-                { 10,    "X" },
-                { 9,     "IX" },
-                { 5,     "V" },
-                { 4,     "IV" },
-                { 1,     "I" },
-            };
-
-        public string ArabicToRoman(int intArabic)
-        {
-            var romanStrBuild = new StringBuilder();
-            string strResult = null;
-
-            //Check if number does not excede maximum value.
-            if (intArabic > 3000)
-                LabelInfo.Content = "Number must be below 3000.";
-
-            //Check if number isn't 0.
-            if (intArabic == 0)
-                strResult = "N";
-            else
-            {
-                foreach (var item in ArabicRomanDict)
-                {
-                    while (intArabic >= item.Key)
-                    {
-                        romanStrBuild.Append(item.Value);
-                        intArabic -= item.Key;
-                    }
-                }
-                strResult = romanStrBuild.ToString();
-            }
-
-            return strResult;
+            ArabicNum1 = Conversion.RomanToArabic(RomanNum1);
+            ArabicNum2 = Conversion.RomanToArabic(RomanNum2);
         }
 
-        public int RomanToArabic(string strRoman)
+        private void ClearAll()
         {
-            LabelInfo.Content = null;
-            int RepCount = 1, intIndex = 0, maxDigitValue = 1000, intTotal = 0;
-            char chrLast = 'Z';
+            InputLabel.Content = null;
+            OutputLabel.Content = null;
+            RomanNum1 = null;
+            RomanNum2 = null;
+            ArabicNum1 = 0;
+            ArabicNum2 = 0;
+            strInputDisplay = null;
+            IsSecondNumber = false;
+        }
 
-            //There was no number for zero. Later on N was used to indicate nothing.
-            if (strRoman == "N")
-                return 0;
-
-            // Rule 4 V,L, and D may only appear once in a roman number sequence.
-            //Use split to split strRoman at the letters and count how many splits there are to check if the letter appears more than once.
-            if (strRoman.Split('V').Length > 2 || strRoman.Split('L').Length > 2
-                || strRoman.Split('D').Length > 2)
+        private void Calculation()
+        {
+            switch (CurrentOperator)
             {
-                LabelInfo.Content += "V, L and D may each only be used once in a sequence.";
+                case Operators.Add:
+                    ArabicResult = ArabicNum1 + ArabicNum2;
+                    break;
+                case Operators.Subtract:
+                    ArabicResult = ArabicNum1 - ArabicNum2;
+                    break;
+                case Operators.Multiply:
+                    ArabicResult = ArabicNum1 * ArabicNum2;
+                    break;
+                case Operators.Divide:
+                    ArabicResult = ArabicNum1 / ArabicNum2;
+                    break;
+                default:
+                    ArabicResult = ArabicNum1;
+                    break;
             }
-                
-
-            //Check rule 1. A single letter may be repeated up to 3 times.
-            foreach (char numeral in strRoman)
-            {
-                if (numeral == chrLast)
-                {
-                    RepCount++;
-                    if (RepCount == 4)
-                    {
-                        LabelInfo.Content += "A single letter may be repeated up to 3 times.";
-                    }
-                }
-                else
-                {
-                    RepCount = 1;
-                    chrLast = numeral;
-                }
-            }
-            //While loop to iterate through Roman string.
-            while (intIndex < strRoman.Length)
-            {
-                //First digit.
-                char chrNum1 = strRoman[intIndex];
-                int iDigit1 = RomanArabicDict[chrNum1];
-
-                //Check for No further numeral or pair may match or exceed the subtracted value. 
-                if (iDigit1 > maxDigitValue)
-                    LabelInfo.Content += "No further numeral or pair may match or exceed prior subtracted values.";
-
-                //Second Digit
-                if (intIndex < strRoman.Length - 1)
-                {
-                    char chrNum2 = strRoman[intIndex + 1];
-                    int iDigit2 = RomanArabicDict[chrNum2];
-                    //Check if second number is greater to see if the first should be subtracted.
-                    if (iDigit2 > iDigit1)
-                    {
-                        //Check if the number subtracted is I, X or C. 
-                        if ("IXVC".IndexOf(chrNum1) == -1)
-                            LabelInfo.Content += "Only I, X or C may be subtracted.";
-                        //Check if the number subtracted is not smaller than a tenth of the number it is subtracted from.
-                        if (iDigit2 > (iDigit1 * 10))
-                            LabelInfo.Content += "The subtracted number must be no smaller than a tenth of the number it is subtracted from.";
-
-                        //MaxDigit Value adjusted to check for rule 3. iDigit1 adjusted according to subtraction rules.
-                        //IntIndex incremented one to skip next digit. (As to not count digit2 twice.)
-                        maxDigitValue = iDigit1 - 1;
-                        iDigit1 = iDigit2 - iDigit1;
-                        intIndex++;
-                    }
-                }
-                //Add digit value to the total result.
-                intTotal += iDigit1;
-
-                //Increment to next Digit.
-                intIndex++;
-            }
-
-            return intTotal;
+            RomanResult = Conversion.ArabicToRoman(ArabicResult);
         }
         #endregion
+        private Operators CurrentOperator;
+
+        private enum Operators
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
 
         #region Button Methods
-        private void Btn_Convert_Click(object sender, RoutedEventArgs e)
+        /*private void Btn_Convert_Click(object sender, RoutedEventArgs e)
         {
             string strInput = TextBxInput.Text.ToUpper();
             TextBxOutput.Text = RomanToArabic(strInput).ToString();
@@ -174,7 +98,45 @@ namespace ItViteaRomeinseRekenmachine
         {
             int intInput = Convert.ToInt32(TextBxOutput.Text);
             TextBxInput.Text = ArabicToRoman(intInput);
+        }*/
+
+        private void Button_ClearAll(object sender, RoutedEventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void Button_Numbers(object sender, RoutedEventArgs e)
+        {
+            Button sendButton = e.Source as Button;
+            string BtnContent = sendButton.Content.ToString();
+
+            if (IsSecondNumber)
+                RomanNum2 += BtnContent;
+            else
+                RomanNum1 += BtnContent;
+            
+            strInputDisplay += BtnContent;
+            InputLabel.Content = strInputDisplay;
+        }
+
+        private void Button_Operators(object sender, RoutedEventArgs e)
+        {
+            Button sendButton = e.Source as Button;
+            string BtnContent = sendButton.Content.ToString();
+            CurrentOperator = (Operators)Enum.Parse(typeof(Operators), sendButton.Name.ToString(), true);
+            IsSecondNumber = true;
+
+            strInputDisplay += String.Format("{0}", BtnContent);
+            InputLabel.Content = strInputDisplay;
+        }
+
+        private void Button_Result(object sender, RoutedEventArgs e)
+        {
+            UpdateNumbers();
+            Calculation();
+            OutputLabel.Content = RomanResult;
         }
         #endregion
+
     }
 }
---
+++ ItViteaRomeinseRekenmachine/NumeralConversion.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItViteaRomeinseRekenmachine
+{
+    public class NumeralConversion : INotifyPropertyChanged
+    {
+        //Private variables.
+        private string _ErrorMsg;
+
+        //Public variables and properties.
+        public string ErrorMsg
+        {
+            get { return _ErrorMsg; }
+            set
+            {
+                value = _ErrorMsg;
+                OnPropertyChanged("ErrorMsg");
+            }
+        }
+
+        public static Dictionary<char, int> RomanArabicDict =
+        new Dictionary<char, int>
+        {
+                {'I',   1 },
+                {'V',   5 },
+                {'X',   10 },
+                {'L',   50 },
+                {'C',   100 },
+                {'D',   500 },
+                {'M',   1000 },
+        };
+        public static Dictionary<int, string> ArabicRomanDict =
+            new Dictionary<int, string>
+            {
+                { 1000,  "M" },
+                { 900,   "CM" },
+                { 500,   "D" },
+                { 400,   "CD" },
+                { 100,   "C" },
+                { 90,    "XC" },
+                { 50,    "L" },
+                { 40,    "XL" },
+                { 10,    "X" },
+                { 9,     "IX" },
+                { 5,     "V" },
+                { 4,     "IV" },
+                { 1,     "I" },
+            };
+
+        #region Methods
+        public string ArabicToRoman(int intArabic)
+        {
+            var romanStrBuild = new StringBuilder();
+            string strResult = null;
+
+            //Check if number does not excede maximum value.
+            if (intArabic > 3000)
+                ErrorMsg = "Number must be below 3000.";
+
+            //Check if number isn't 0.
+            if (intArabic == 0)
+                strResult = "N";
+            else
+            {
+                foreach (var item in ArabicRomanDict)
+                {
+                    while (intArabic >= item.Key)
+                    {
+                        romanStrBuild.Append(item.Value);
+                        intArabic -= item.Key;
+                    }
+                }
+                strResult = romanStrBuild.ToString();
+            }
+
+            return strResult;
+        }
+
+        public int RomanToArabic(string strRoman)
+        {
+            ErrorMsg = null;
+            int RepCount = 1, intIndex = 0, maxDigitValue = 1000, intTotal = 0;
+            char chrLast = 'Z';
+
+            //There was no number for zero. Later on N was used to indicate nothing.
+            if (strRoman == "N")
+                return 0;
+
+            // Rule 4 V,L, and D may only appear once in a roman number sequence.
+            //Use split to split strRoman at the letters and count how many splits there are to check if the letter appears more than once.
+            if (strRoman.Split('V').Length > 2 || strRoman.Split('L').Length > 2
+                || strRoman.Split('D').Length > 2)
+            {
+                ErrorMsg += "V, L and D may each only be used once in a sequence.";
+            }
+
+
+            //Check rule 1. A single letter may be repeated up to 3 times.
+            foreach (char numeral in strRoman)
+            {
+                if (numeral == chrLast)
+                {
+                    RepCount++;
+                    if (RepCount == 4)
+                    {
+                        ErrorMsg += "A single letter may be repeated up to 3 times.";
+                    }
+                }
+                else
+                {
+                    RepCount = 1;
+                    chrLast = numeral;
+                }
+            }
+            //While loop to iterate through Roman string.
+            while (intIndex < strRoman.Length)
+            {
+                //First digit.
+                char chrNum1 = strRoman[intIndex];
+                int iDigit1 = RomanArabicDict[chrNum1];
+
+                //Check for No further numeral or pair may match or exceed the subtracted value. 
+                if (iDigit1 > maxDigitValue)
+                    ErrorMsg += "No further numeral or pair may match or exceed prior subtracted values.";
+
+                //Second Digit
+                if (intIndex < strRoman.Length - 1)
+                {
+                    char chrNum2 = strRoman[intIndex + 1];
+                    int iDigit2 = RomanArabicDict[chrNum2];
+                    //Check if second number is greater to see if the first should be subtracted.
+                    if (iDigit2 > iDigit1)
+                    {
+                        //Check if the number subtracted is I, X or C. 
+                        if ("IXVC".IndexOf(chrNum1) == -1)
+                            ErrorMsg += "Only I, X or C may be subtracted.";
+                        //Check if the number subtracted is not smaller than a tenth of the number it is subtracted from.
+                        if (iDigit2 > (iDigit1 * 10))
+                            ErrorMsg += "The subtracted number must be no smaller than a tenth of the number it is subtracted from.";
+
+                        //MaxDigit Value adjusted to check for rule 3. iDigit1 adjusted according to subtraction rules.
+                        //IntIndex incremented one to skip next digit. (As to not count digit2 twice.)
+                        maxDigitValue = iDigit1 - 1;
+                        iDigit1 = iDigit2 - iDigit1;
+                        intIndex++;
+                    }
+                }
+                //Add digit value to the total result.
+                intTotal += iDigit1;
+
+                //Increment to next Digit.
+                intIndex++;
+            }
+
+            return intTotal;
+        }
+#endregion
+
+        #region INotifyPropertyChanged Members  
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
