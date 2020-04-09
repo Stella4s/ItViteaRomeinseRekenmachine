d8809aa8f53d40bf7480528827d58c2ca766c6a2
Stella van der burg <estellavanderburg@gmail.com>
2020-04-09 10:34:44 +0200

    Changed the manner in which the roman numerals are extracted from the equation. Added the method to make the Undo button fuctional.

--- ItViteaRomeinseRekenmachine/MainWindow.xaml.cs
+++ ItViteaRomeinseRekenmachine/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace ItViteaRomeinseRekenmachine
     public partial class MainWindow : Window
     {
         #region private Variables
-        private string RomanNum1 = null, RomanNum2, strInputDisplay, RomanResult;
+        private string RomanNum1 = null, RomanNum2, strInputDisplay, strCalculation, RomanResult;
         private int ArabicNum1 = 0, ArabicNum2, ArabicResult;
         private bool IsSecondNumber = false;
         private NumeralConversion Conversion;
@@ -36,8 +36,22 @@ namespace ItViteaRomeinseRekenmachine
         /// Methods that aren't directly tied to any object handler, but instead serve to support those methods.
         /// </summary>
         #region Support Methods
+        //Private Variables specific for support methods.
+        private static char[] chrOperators = {'+', '-' ,'*', '/'};
+
         private void UpdateNumbers()
         {
+            ArabicNum1 = Conversion.RomanToArabic(RomanNum1);
+            ArabicNum2 = Conversion.RomanToArabic(RomanNum2);
+        }
+        private void GetNumbers()
+        {
+            //Get the position of the math operator and use that to get both Roman numbers.
+            int intOprtPosition = strCalculation.IndexOfAny(chrOperators);
+            RomanNum1 = strCalculation.Substring(0, intOprtPosition);
+            RomanNum2 = strCalculation.Substring(intOprtPosition + 1);
+
+            //Conversion from Roman to Arabic.
             ArabicNum1 = Conversion.RomanToArabic(RomanNum1);
             ArabicNum2 = Conversion.RomanToArabic(RomanNum2);
         }
@@ -51,6 +65,7 @@ namespace ItViteaRomeinseRekenmachine
             ArabicNum1 = 0;
             ArabicNum2 = 0;
             strInputDisplay = null;
+            strCalculation = null;
             IsSecondNumber = false;
         }
 
@@ -75,6 +90,19 @@ namespace ItViteaRomeinseRekenmachine
                     break;
             }
             RomanResult = Conversion.ArabicToRoman(ArabicResult);
+        }
+
+        private void AddToCalcAndDisplay(string str)
+        {
+            strCalculation += str;
+            strInputDisplay += str;
+            InputLabel.Content = strInputDisplay;
+        }
+        private void AddToCalcAndDisplay(string strCalc, string strDisplay)
+        {
+            strCalculation += strCalc;
+            strInputDisplay += strDisplay;
+            InputLabel.Content = strInputDisplay;
         }
         #endregion
         private Operators CurrentOperator;
@@ -110,29 +138,62 @@ namespace ItViteaRomeinseRekenmachine
             Button sendButton = e.Source as Button;
             string BtnContent = sendButton.Content.ToString();
 
-            if (IsSecondNumber)
-                RomanNum2 += BtnContent;
-            else
-                RomanNum1 += BtnContent;
-            
-            strInputDisplay += BtnContent;
-            InputLabel.Content = strInputDisplay;
+            /* if (IsSecondNumber)
+                 RomanNum2 += BtnContent;
+             else
+                 RomanNum1 += BtnContent;*/
+
+            AddToCalcAndDisplay(BtnContent);
         }
 
         private void Button_Operators(object sender, RoutedEventArgs e)
         {
             Button sendButton = e.Source as Button;
             string BtnContent = sendButton.Content.ToString();
-            CurrentOperator = (Operators)Enum.Parse(typeof(Operators), sendButton.Name.ToString(), true);
-            IsSecondNumber = true;
+            string BtnName = sendButton.Name.ToString();
+            string strForCalc = "";
 
-            strInputDisplay += String.Format("{0}", BtnContent);
-            InputLabel.Content = strInputDisplay;
+            //CurrentOperator = (Operators)Enum.Parse(typeof(Operators), sendButton.Name.ToString(), true);
+            switch (BtnName)
+            {
+                case "Add":
+                    CurrentOperator = Operators.Add;
+                    strForCalc = "+";
+                    break;
+                case "Subtract":
+                    CurrentOperator = Operators.Subtract;
+                    strForCalc = "-";
+                    break;
+                case "Multiply":
+                    CurrentOperator = Operators.Multiply;
+                    strForCalc = "*";
+                    break;
+                case "Divide":
+                    CurrentOperator = Operators.Divide;
+                    strForCalc = "/";
+                    break;
+            }
+            //IsSecondNumber = true;
+
+            AddToCalcAndDisplay(strForCalc, BtnContent);
+            //strInputDisplay += String.Format("{0}", BtnContent);
+            //InputLabel.Content = strInputDisplay;
+        }
+
+        private void Button_Undo(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(strCalculation) == false && string.IsNullOrEmpty(strInputDisplay) == false)
+            {
+                int TempLDisplay = (strInputDisplay.Length);
+                strInputDisplay = strInputDisplay.Remove(TempLDisplay - 1);
+                strCalculation = strCalculation.Remove(TempLDisplay - 1);
+                InputLabel.Content = strInputDisplay;
+            }
         }
 
         private void Button_Result(object sender, RoutedEventArgs e)
         {
-            UpdateNumbers();
+            GetNumbers();
             Calculation();
             OutputLabel.Content = RomanResult;
         }
